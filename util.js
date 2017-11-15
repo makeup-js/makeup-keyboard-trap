92808a2633d90ee3833f145a08ba512140b872e0
Scott Davis <scodavis@ebay.com>
2017-11-15 14:16:02 -0500

    check in files generated from commit hook

--- util.js
+++ util.js
@@ -2,17 +2,25 @@
 
 var focusableElementsList = ['a[href]', 'button:not([disabled])', 'area[href]', 'input:not([disabled])', 'select:not([disabled])', 'textarea:not([disabled])', 'iframe', 'object', 'embed', '*[tabindex]', '*[contenteditable]'];
 
+// when bundled up with isomorphic components on the server, this code is run, so we must check if
+// 'document' is defined. When it is not, on the server, we return a no-op there. Since addEventListener
+// is loaded and called in index.js on the server, we include that method as a no-op as well.
+var NOOP = { addEventListener: function addEventListener() {} };
 var trapBoundary = void 0;
-if (typeof document !== "undefined") {
+
+var getTrapBoundary = function getTrapBoundary() {
+    if (trapBoundary) return trapBoundary.cloneNode();
+    if (typeof document === "undefined") return NOOP;
+
     trapBoundary = document.createElement('div');
     trapBoundary.setAttribute('tabindex', '0');
     trapBoundary.className = 'keyboard-trap-boundary';
-}
 
-var noop = { addEventListener: function addEventListener() {} };
+    return trapBoundary;
+};
 
 function createTrapBoundary() {
-    return trapBoundary ? trapBoundary.cloneNode() : noop;
+    return getTrapBoundary();
 }
 
 module.exports = {
