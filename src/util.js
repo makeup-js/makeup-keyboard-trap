fa2eac3dd696f32be3a09057fb5d93e132e7f1a4
Scott Davis <scodavis@ebay.com>
2017-11-15 14:14:42 -0500

    create node lazily, add comments

--- src/util.js
+++ src/util.js
@@ -14,17 +14,25 @@ const focusableElementsList = [
     '*[contenteditable]'
 ];
 
+// when bundled up with isomorphic components on the server, this code is run, so we must check if
+// 'document' is defined. When it is not, on the server, we return a no-op there. Since addEventListener
+// is loaded and called in index.js on the server, we include that method as a no-op as well.
+const NOOP = { addEventListener: () => {} };
 let trapBoundary;
-if (typeof document !== "undefined") {
+
+const getTrapBoundary = () => {
+    if (trapBoundary) return trapBoundary.cloneNode();
+    if (typeof document === "undefined") return NOOP;
+
     trapBoundary = document.createElement('div');
     trapBoundary.setAttribute('tabindex', '0');
     trapBoundary.className = 'keyboard-trap-boundary';
-}
 
-const noop = { addEventListener: () => {} };
+    return trapBoundary;
+};
 
 function createTrapBoundary() {
-    return trapBoundary ? trapBoundary.cloneNode() : noop;
+    return getTrapBoundary();
 }
 
 module.exports = {
